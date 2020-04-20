65d82d818904e79a7b1508f9c6a6e0d73b816f1a
ms880525 <44563860+ms880525@users.noreply.github.com>
2020-04-20 22:41:24 +0800

    2020040 upfate

--- js/index.js
+++ js/index.js
@@ -1,38 +1,28 @@
 $(document).ready(function() {
-    $('html').fadeOut(0).fadeIn(1500);
+    $('body').fadeOut(100).fadeIn(1500);
 /*gotop*/
 	$('#BackTop').click(function(){ 
 		$('html,body').animate({scrollTop:0}, 333);
 	});
-    /*navfadeInOut*/
-    $(window).scroll(function() {
-      if ( $(this).scrollTop() > 200 ){
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 200 ){
 			$('#BackTop').fadeIn(222);
 		} else {
 			$('#BackTop').stop().fadeOut(222);
 		}
-        
+	}).scroll(); 
+    
+    /*$(window).scroll(function() {
 		if ( $(this).scrollTop() > 350 ){
-			$('nav').fadeIn(400);
-		} else  {
-			$('nav').stop().fadeOut(0);
+			$('nav').fadeIn(222);
+		} else {
+			$('nav').stop().fadeOut(222);
             $('nav').css({
               'background':'#EFEFF0',
             })
 		}
-        /*fadeInOut*/
-        $('.hideme').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
-                $(this).addClass('showme');
-            }
-            if( bottom_of_window < bottom_of_object ){
-                $(this).removeClass('showme');
-            }
-        });
+	}).scroll();*/
     
-	}).scroll();
 });
 
 
